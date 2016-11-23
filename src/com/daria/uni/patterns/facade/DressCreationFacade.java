8d4c0be37c937ed1435815b58e2a233d53c95eb0
Daria Pindus <daria.pindus@gmail.com>
2016-11-24 01:24:08 +0200

    added facade methods

--- src/com/daria/uni/patterns/facade/DressCreationFacade.java
+++ src/com/daria/uni/patterns/facade/DressCreationFacade.java
@@ -6,5 +6,20 @@ import com.daria.uni.patterns.facade.classes.Factory;
 import com.daria.uni.patterns.facade.classes.Tailor;
 
 public class DressCreationFacade {
-
+    Designer designer;
+    Tailor tailor;
+    Factory factory;
+    public DressCreationFacade(Designer designer, Tailor tailor, Factory factory) {
+        this.designer = designer;
+        this.tailor = tailor;
+        this.factory = factory;
+    }
+    public void createDress() {
+        designer.createDesign();
+        factory.getFabric();
+        tailor.takeMeasurements();
+        designer.changeDesign();
+        tailor.sewDress();
+        tailor.makeFitting();
+    }
 }
